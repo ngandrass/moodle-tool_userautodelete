1103ac84435be40bf7144a04ef6501d31c6ea4f3
Niels Gandraß <niels@gandrass.de>
2025-05-07 14:48:36 +0200

    Create basic logging wrapper

---
+++ classes/logger.php
@@ -0,0 +1,79 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * This file defines the logger class
+ *
+ * @package   tool_userautodelete
+ * @copyright 2025 Niels Gandraß <niels@gandrass.de>
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+namespace tool_userautodelete;
+
+// @codingStandardsIgnoreLine
+defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
+
+
+/**
+ * Handles logging
+ *
+ * This is currently just an mtrace() wrapper but allows for future extension
+ * of the logging system.
+ */
+class logger {
+
+    /**
+     * Logs a debug message
+     *
+     * @param string $message The message to log
+     * @return void
+     */
+    public static function debug(string $message): void {
+        mtrace("[DEBUG] $message");
+    }
+
+    /**
+     * Logs an info message
+     *
+     * @param string $message The message to log
+     * @return void
+     */
+    public static function info(string $message): void {
+        mtrace("[INFO] $message");
+    }
+
+    /**
+     * Logs a warning message
+     *
+     * @param string $message The message to log
+     * @return void
+     */
+    public static function warning(string $message): void {
+        mtrace("[WARN] $message");
+    }
+
+    /**
+     * Logs an error message
+     *
+     * @param string $message The message to log
+     * @return void
+     */
+    public static function error(string $message): void {
+        mtrace("[ERROR] $message");
+    }
+
+}
