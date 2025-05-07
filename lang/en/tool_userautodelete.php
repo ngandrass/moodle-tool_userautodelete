d863430ee811736ee20cd9e1edd4be13b80d4120
Niels Gandraß <niels@gandrass.de>
2025-05-07 11:55:19 +0200

    Create required admin settings

--- lang/en/tool_userautodelete.php
+++ lang/en/tool_userautodelete.php
@@ -26,4 +26,41 @@
 // @codingStandardsIgnoreLine
 defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
 
+// General.
 $string['pluginname'] = 'Automatic User Deletion';
+$string['privacy:metadata'] = 'TODO';
+$string['task_check_and_delete_users'] = 'Check and delete users';
+
+// Admin settings.
+$string['setting_enable'] = 'Enable plugin';
+$string['setting_enable_desc'] = 'Enables or disables the plugin globally. If this is disabled, no action will be performed.';
+$string['setting_ignore_siteadmins'] = 'Ignore site admins';
+$string['setting_ignore_siteadmins_desc'] = 'If enabled, all users that are global site admins will never be deleted.';
+$string['setting_ignore_roles'] = 'Ignored roles';
+$string['setting_ignore_roles_desc'] = 'If enabled, all users that have one of the selected roles will never be deleted.';
+
+$string['setting_header_user_deletion'] = 'User Deletion';
+$string['setting_header_user_deletion_desc'] = 'Configuration of the automatic user deletion.';
+$string['setting_delete_threshold_days'] = 'Deletion threshold';
+$string['setting_delete_threshold_days_desc'] = 'The number of days of inactivity after which a user will be deleted. The deletion will only be performed if the user has not logged in for this number of days.';
+$string['setting_delete_email_enable'] = 'Deletion notifications';
+$string['setting_delete_email_enable_desc'] = 'If enabled, users will receive a final mail and are immediately deleted afterwards. This can be used to send confirmations of user deletion.';
+$string['setting_delete_email_subject'] = 'Mail subject';
+$string['setting_delete_email_subject_desc'] = 'The subject of the deletion notification email.';
+$string['setting_delete_email_subject_default'] = 'Your account was deleted';
+$string['setting_delete_email_body'] = 'Mail body';
+$string['setting_delete_email_body_desc'] = 'The body of the deletion notification email.';
+$string['setting_delete_email_body_default'] = '<h1>TODO TODO TODO!</h1>Your account was deleted :(';
+
+$string['setting_header_deletion_warning'] = 'Deletion Warnings';
+$string['setting_header_deletion_warning_desc'] = 'Configuration of the deletion warning emails. Warning emails are sent a number of days before the user is deleted. This informs users that their account will be deleted soon and gives them time to perform a login to keep their accounts active.';
+$string['setting_warning_email_enable'] = 'Deletion warnings';
+$string['setting_warning_email_enable_desc'] = 'If enabled, users will receive a warning email a configurable number of days before they are deleted.';
+$string['setting_warning_threshold_days'] = 'Warning threshold';
+$string['setting_warning_threshold_days_desc'] = 'The number of days before the user is deleted that the warning email will be sent. This is relative to the deletion threshold.';
+$string['setting_warning_email_subject'] = 'Mail subject';
+$string['setting_warning_email_subject_desc'] = 'The subject of the warning email.';
+$string['setting_warning_email_subject_default'] = 'Your account will be deleted soon';
+$string['setting_warning_email_body'] = 'Mail body';
+$string['setting_warning_email_body_desc'] = 'The body of the warning email.';
+$string['setting_warning_email_body_default'] = '<h1>TODO TODO TODO!</h1>Your account will be deleted soon! Please login to keep your account active.';
--- settings.php
+++ settings.php
@@ -28,9 +28,99 @@ defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
 
 if ($hassiteconfig) {
     $settings = new admin_settingpage('tool_userautodelete_settings', new lang_string('pluginname', 'tool_userautodelete'));
+    $ADMIN->add('tools', $settings);
 
     // phpcs:ignore Generic.CodeAnalysis.EmptyStatement.DetectedIf
     if ($ADMIN->fulltree) {
-        // TO-DO: Define actual plugin settings page and add it to the tree - {@link https://docs.moodle.org/dev/Admin_settings}.
+        // Enable plugin globally.
+        $settings->add(new admin_setting_configcheckbox('tool_userautodelete/enable',
+            get_string('setting_enable', 'tool_userautodelete'),
+            get_string('setting_enable_desc', 'tool_userautodelete'),
+            '0'
+        ));
+
+        // Exclude site admins.
+        $settings->add(new admin_setting_configcheckbox('tool_userautodelete/ignore_siteadmins',
+            get_string('setting_ignore_siteadmins', 'tool_userautodelete'),
+            get_string('setting_ignore_siteadmins_desc', 'tool_userautodelete'),
+            '1'
+        ));
+
+        // Excluded roles.
+        $settings->add(new admin_setting_pickroles('tool_userautodelete/ignore_roles',
+            get_string('setting_ignore_roles', 'tool_userautodelete'),
+            get_string('setting_ignore_roles_desc', 'tool_userautodelete'),
+            []
+        ));
+
+        // Header: Automatic user deletion.
+        $settings->add(new admin_setting_heading('tool_userautodelete/header_user_deletion',
+            get_string('setting_header_user_deletion', 'tool_userautodelete'),
+            get_string('setting_header_user_deletion_desc', 'tool_userautodelete')
+        ));
+
+        // Deletion threshold.
+        $settings->add(new admin_setting_configtext('tool_userautodelete/delete_threshold_days',
+            get_string('setting_delete_threshold_days', 'tool_userautodelete'),
+            get_string('setting_delete_threshold_days_desc', 'tool_userautodelete'),
+            '1095', // 3 years.
+            PARAM_INT
+        ));
+
+        // Enable deletion notifications.
+        $settings->add(new admin_setting_configcheckbox('tool_userautodelete/delete_email_enable',
+            get_string('setting_delete_email_enable', 'tool_userautodelete'),
+            get_string('setting_delete_email_enable_desc', 'tool_userautodelete'),
+            '0'
+        ));
+
+        // Deletion email subject.
+        $settings->add(new admin_setting_configtext('tool_userautodelete/delete_email_subject',
+            get_string('setting_delete_email_subject', 'tool_userautodelete'),
+            get_string('setting_delete_email_subject_desc', 'tool_userautodelete'),
+            get_string('setting_delete_email_subject_default', 'tool_userautodelete')
+        ));
+
+        // Deletion email body.
+        $settings->add(new admin_setting_configtextarea('tool_userautodelete/delete_email_body',
+            get_string('setting_delete_email_body', 'tool_userautodelete'),
+            get_string('setting_delete_email_body_desc', 'tool_userautodelete'),
+            get_string('setting_delete_email_body_default', 'tool_userautodelete')
+        ));
+
+        // Header: Deletion warnings.
+        $settings->add(new admin_setting_heading('tool_userautodelete/header_deletion_warning',
+            get_string('setting_header_deletion_warning', 'tool_userautodelete'),
+            get_string('setting_header_deletion_warning_desc', 'tool_userautodelete')
+        ));
+
+        // Enable warnings.
+        $settings->add(new admin_setting_configcheckbox('tool_userautodelete/warning_email_enable',
+            get_string('setting_warning_email_enable', 'tool_userautodelete'),
+            get_string('setting_warning_email_enable_desc', 'tool_userautodelete'),
+            '1'
+        ));
+
+        // Warning threshold.
+        $settings->add(new admin_setting_configtext('tool_userautodelete/warning_threshold_days',
+            get_string('setting_warning_threshold_days', 'tool_userautodelete'),
+            get_string('setting_warning_threshold_days_desc', 'tool_userautodelete'),
+            '30', // 30 days.
+            PARAM_INT
+        ));
+
+        // Warning email subject.
+        $settings->add(new admin_setting_configtext('tool_userautodelete/warning_email_subject',
+            get_string('setting_warning_email_subject', 'tool_userautodelete'),
+            get_string('setting_warning_email_subject_desc', 'tool_userautodelete'),
+            get_string('setting_warning_email_subject_default', 'tool_userautodelete')
+        ));
+
+        // Warning email body.
+        $settings->add(new admin_setting_configtextarea('tool_userautodelete/warning_email_body',
+            get_string('setting_warning_email_body', 'tool_userautodelete'),
+            get_string('setting_warning_email_body_desc', 'tool_userautodelete'),
+            get_string('setting_warning_email_body_default', 'tool_userautodelete'),
+        ));
     }
 }
