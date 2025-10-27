de03d6220d78edf37b83d80e50bf7096db598721
Niels Gandraß <niels@gandrass.de>
2025-10-27 16:04:16 +0100

    Switch to automatically updated shields for Moodle and PHP version support

--- version.php
+++ version.php
@@ -29,5 +29,5 @@ $plugin->component = 'tool_userautodelete';
 $plugin->release = '1.4.0';
 $plugin->version = 2025101000;
 $plugin->requires = 2022112800;
-$plugin->supported = [401, 501];
+$plugin->supported = [401, 501]; // X meta-supported-moodle{4.1 - 5.1} meta-supported-php{7.4 - 8.4}.
 $plugin->maturity = MATURITY_STABLE;
