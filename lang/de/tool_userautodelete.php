80b47b8739b7bfe6a9ba6070cd4ed9557f0baf70
Niels Gandraß <niels@gandrass.de>
2026-03-26 14:59:36 +0100

    Purge unused language strings

--- lang/de/tool_userautodelete.php
+++ lang/de/tool_userautodelete.php
@@ -69,102 +69,23 @@ $string['defaultworkflow_deletemail_message'] = '<p>Hallo,</p><p>Ihr Konto auf u
 
 // Task execution.
 $string['plugin_disabled_skipping_execution'] = 'Das Plugin ist global deaktiviert, überspringe Ausführung.';
-$string['warning_email_disabled'] = 'Warn-E-Mails sind deaktiviert';
-$string['warning_email_disabled_skipping'] = 'Warn-E-Mails sind deaktiviert, überspringe ...';
-$string['warning_email_sent_to_user'] = 'Warn-E-Mail an Nutzer mit ID {$a} gesendet';
-$string['delete_email_sent_to_user'] = 'Löschungsbenachrichtigung an Nutzer mit ID {$a} gesendet';
-$string['user_anonymized'] = 'Nutzer mit ID {$a} wurde anonymisiert';
-$string['user_deleted'] = 'Nutzer mit ID {$a} wurde gelöscht';
-$string['user_recovered'] = 'Nutzer mit ID {$a} war zuvor als inaktiv markiert, ist aber zurückgekehrt. Nutzer wird nicht gelöscht.';
 $string['users_to_warn'] = 'Zu warnende Nutzer';
-$string['users_to_warn_a'] = '{$a} Nutzer für den Versand einer Warn-E-Mail gefunden.';
-$string['no_users_to_warn'] = 'Keine Nutzer für den Versand einer Warn-E-Mail gefunden.';
 $string['users_to_delete'] = 'Zu löschende Nutzer';
-$string['users_to_delete_a'] = '{$a} Nutzer zur Löschung gefunden.';
-$string['no_users_to_delete'] = 'Keine Nutzer zur Löschung gefunden.';
 
 // Admin settings.
 $string['setting_plugin_desc'] = 'Dieses Plugin löscht automatisch Nutzer die sich für eine konfigurierbare Anzahl von Tagen nicht eingeloggt haben. Dies ist nützlich, um die Moodle-Datenbank sauber zu halten und inaktive Nutzerkonten zu entfernen. Das Plugin kann so konfiguriert werden, dass inaktive Nutzer einige Tage vor der Kontolöschung eine Warn-E-Mail erhalten. Dies gibt Nutzern die Möglichkeit, sich erneut einzuloggen und ihre Konten zu behalten. Das Plugin unterstützt außerdem das Löschen von Nutzern auf DSGVO-konforme Weise, sodass keine personenbezogene Daten in der Datenbank verbleiben.';
 $string['setting_enable'] = 'Plugin aktivieren';
 $string['setting_enable_desc'] = 'Aktiviert oder deaktiviert das Plugin global. Wenn dies deaktiviert ist, werden keine Aktionen ausgeführt.';
-$string['setting_ignore_auths'] = 'Ignorierte Authentifizierungswege';
-$string['setting_ignore_auths_desc'] = 'Alle Nutzer die einen der ausgewählten Authentifizierungswege verwenden, werden niemals gelöscht. Beachten Sie, dass hier nur aktivierte Authentifizierungs-Plugins aufgelistet sind.';
-$string['setting_ignore_siteadmins'] = 'Administratoren ignorieren';
-$string['setting_ignore_siteadmins_desc'] = 'Globale Administratoren können nicht gelöscht werden. Entfernen Sie die Administratorrolle, um sie in den automatischen Löschprozess einzubeziehen.';
-$string['setting_ignore_roles'] = 'Ignorierte Rollen';
-$string['setting_ignore_roles_desc'] = 'Alle Nutzer die mindestens einer der ausgewählten Rollen zugewiesen sind werden niemals gelöscht.';
-$string['setting_dryrun'] = 'Probelauf';
-$string['setting_dryrun_desc'] = 'Ein Probelauf listet alle Benutzer auf, die bei der nächsten Inaktivitätsprüfung eine Warnmeldung erhalten oder gelöscht werden würden. Dies ist nützlich, um zu überprüfen, ob das Plugin korrekt konfiguriert ist und um zu sehen, welche Benutzer betroffen wären. Der Probelauf führt keinerlei Aktionen durch und ist daher zerstörungsfrei!';
-$string['setting_dryrun_button'] = 'Probelauf durchführen';
-$string['setting_suspended_only'] = 'Nur gesperrte Nutzer';
-$string['setting_suspended_only_desc'] = 'Wenn aktiviert, werden nur zurzeit gesperrte Nutzer mit einbezogen. Alle aktiven Nutzer bleiben unangetastet bis diese Option deaktiviert wird. Dies kann nützlich sein wenn der Account-Lebenszyklus extern verwaltet wird (z.B. über SSO) oder eine Sperrphase existiert, bevor der Nutzer abschließend gelöscht werden.';
 $string['setting_task_execution_interval'] = 'Prüfintervall';
 $string['setting_task_execution_interval_desc'] = 'Die Überprüfung auf inaktive Nutzer wird über einen geplante Task durchgeführt, der über den Moodle-Cron ausgeführt wird. Sie können das Intervall, in dem dieser Task ausgeführt wird, über den folgenden Button konfigurieren.';
 $string['setting_task_execution_interval_button'] = 'Prüfintervall konfigurieren';
-$string['setting_task_logs'] = 'Logdaten';
-$string['setting_task_logs_desc'] = 'Dieses Plugin protokolliert alle Aktionen, z.B. Nutzerlöschung, die es ausführt. Die Protokolle aller Aktionen die bei vorherigen Prüfungen durchgeführt wurden, können über den folgenden Button eingesehen werden.';
-$string['setting_task_logs_button'] = 'Logdaten anzeigen';
 
-$string['setting_header_user_deletion'] = 'Nutzerlöschung';
-$string['setting_header_user_deletion_desc'] = 'Konfiguration der automatischen Nutzerlöschung. Nutzer, die sich für mindestens die konfigurierte Anzahl von Tagen nicht eingeloggt haben und nicht durch die obigen Einstellungen ausgeschlossen sind, werden automatisch gelöscht.';
-$string['setting_delete_threshold_days'] = 'Löschschwelle';
-$string['setting_delete_threshold_days_desc'] = 'Die Anzahl der Tage seit dem letzten Login, nach denen ein Nutzer gelöscht wird. Die Löschung erfolgt nur, wenn sich der Nutzer für diese Anzahl von Tagen nicht eingeloggt hat.';
-$string['setting_anonymize_user_data'] = 'Gelöschte Nutzer anonymisieren';
-$string['setting_anonymize_user_data_desc'] = 'Beim Löschen eines Nutzerkontos behält Moodle einzelne Teile der Nutzerdaten in seiner Datenbank. Dies beinhaltet unter anderem Vorname, Nachname, E-Mail-Adresse, letzte IP-Adresse und andere sensible Informationen. Dies kann je nach Ihren Datenschutzbestimmungen/-auflagen oder der lokalen Rechtsauslegung ein Problem darstellen. Wenn diese Einstellung aktiviert ist, werden alle verbleibenden Nutzerdaten anonymisiert, wodurch die Möglichkeit, ein gelöschtes Nutzerkonto dem vorherigen Besitzer zuzuordnen, vollständig eliminiert wird.';
-$string['setting_delete_email_enable'] = 'Löschbenachrichtigungen';
-$string['setting_delete_email_enable_desc'] = 'Wenn aktiviert, erhalten Nutzer direkt vor der Kontolöschung eine abschließende E-Mail und werden anschließend sofort gelöscht. Dies kann verwendet werden, um Bestätigungen der Nutzerlöschung zu senden.';
-$string['setting_delete_email_subject'] = 'Betreff';
-$string['setting_delete_email_subject_desc'] = 'Der Betreff der Löschbenachrichtigungs-E-Mail.';
-$string['setting_delete_email_subject_default'] = 'Ihr Konto wurde gelöscht';
-$string['setting_delete_email_body'] = 'Inhalt';
-$string['setting_delete_email_body_desc'] = 'Der Inhalt der Löschbenachrichtigungs-E-Mail.';
-$string['setting_delete_email_body_default'] = '<p>Hallo,</p><p>Ihr Konto auf unserer Seite wurde aufgrund von Inaktivität gelöscht. Wenn Sie unseren Dienst weiterhin nutzen möchten, erstellen Sie sich bitte ein neues Konto.</p><p>Mit freundlichen Grüßen</p>';
 
-$string['setting_header_deletion_warning'] = 'Löschwarnungen';
-$string['setting_header_deletion_warning_desc'] = 'Konfiguration der Löschwarnung-E-Mails. Warnungs-E-Mails werden einige Tage vor der inaktivitätsbedingten Löschung eines Nutzers gesendet. Diese informieren Nutzer darüber, dass ihr Konto bald gelöscht wird, und gibt ihnen Zeit, sich einzuloggen um ihre Konten vor der Löschung zu bewahren.';
-$string['setting_warning_email_enable'] = 'Löschwarnungen';
-$string['setting_warning_email_enable_desc'] = 'Wenn aktiviert, erhalten Nutzer eine Anzahl an Tagen vor ihrer Löschung eine Warn-E-Mail.';
-$string['setting_warning_threshold_days'] = 'Warnschwelle';
-$string['setting_warning_threshold_days_desc'] = 'Die Anzahl der Tage vor der Löschung des Nutzers, an denen die Löschwarnung per E-Mail gesendet wird. Dies ist relativ zur Löschschwelle.';
-$string['setting_warning_email_subject'] = 'Betreff';
-$string['setting_warning_email_subject_desc'] = 'Der Betreff der Warn-E-Mail.';
-$string['setting_warning_email_subject_default'] = 'Ihr Konto wird bald gelöscht - Handlung erforderlich!';
-$string['setting_warning_email_body'] = 'Inhalt';
-$string['setting_warning_email_body_desc'] = 'Der Inhalt der Warn-E-Mail.';
-$string['setting_warning_email_body_default'] = '<p>Hallo,</p><p>Ihr Konto auf unserer Seite war für einen langen Zeitraum inaktiv. Um Ihr Konto zu behalten, <strong>loggen Sie sich jetzt ein, um zu verhindern, dass Ihr Konto gemäß unserer Datenschutzrichtlinie in den nächsten Tagen gelöscht wird</strong>.</p><p>Wenn Sie möchten, dass Ihr Konto gelöscht wird, können Sie diese Nachricht ignorieren.</p><p>Mit freundlichen Grüßen</p>';
 
 // Page: info / dryrun
 $string['dry_run'] = 'Probelauf';
-$string['page_title_dryrun'] = 'Automatische Nutzerlöschung (Probelauf)';
 $string['dry_run_explanation'] = 'Diese Seite zeigt die Aktionen, die während der nächsten Inaktivitätsprüfung ausgeführt werden würden. Keine der Aktionen wurde tatsächlich ausgeführt!';
-$string['current_configuration'] = 'Aktuelle Konfiguration';
-$string['current_configuration_table_desc'] = 'Diese Tabelle listet den Zeitpunkt der nächsten Inaktivitätsprüfung die im Hintergrund ausgeführt werden würde, sowie die konfigurierten Schwellenwerte für Warn-E-Mails und Nutzerlöschungen auf. Außerdem ist angegeben, welche Nutzerrollen von der automatischen Löschung ausgeschlossen sind.';
-$string['dry_run_affected_users_desc'] = 'Die hier aufgeführten Nutzer würden entweder eine Warn-E-Mail erhalten oder gelöscht werden. Wenn keine Nutzer aufgelistet sind, hat sich kein Nutzer länger als die konfigurierten Schwellenwerte nicht eingeloggt oder der Nutzer ist gemäß der Plugin-Konfiguration ausgeschlossen. Konten die zwar erstellt wurden, bei denen sich jedoch noch nie ein Benutzer angemeldet hat, werden mit ihrem Erstellungsdatum <i>„Erstellungsdatum“</i> anstelle des letzten Anmeldedatums <i>„Letzter Zugriff“</i> gelistet.';
-$string['a_users_would_be_deleted'] = 'Die folgenden {$a} Nutzer würden gelöscht werden';
-$string['a_users_would_be_warned'] = 'Die folgenden {$a} Nutzer würden eine Warn-E-Mail erhalten';
 
 // Page: action logs
-$string['action_log'] = 'Aktionsprotokoll';
-$string['action_log_explanation'] = 'Diese Seite listet alle Aktionen auf, die von diesem Plugin durchgeführt wurden. Jedes Mal, wenn eine Warn-E-Mail gesendet wird, ein Nutzer endgültig gelöscht wird oder ein Nutzer nach einer Warn-E-Mail zurückgekehrt ist, wird ein Eintrag im Aktionsprotokoll erstellt. Inaktivitätsprüfungen bei denen keinerlei Aktionen durchgeführt wurden, werden nicht protokolliert.';
-$string['inspecting_raw_logs'] = 'Technische Logdaten anzeigen';
-$string['latest_actions'] = 'Letzte Aktionen';
-$string['latest_actions_desc'] = 'Diese Tabelle listet die Aktionen auf, die zuletzt von diesem Plugin durchgeführt wurden. Inaktivitätsprüfungen bei denen keine Aktionen durchgeführt wurden werden nicht aufgeführt.';
-$string['page_title_action_log'] = 'Automatische Nutzerlöschung (Aktionsprotokoll)';
-$string['view_scheduled_task_logs'] = 'Technische Logdaten anzeigen';
-$string['view_scheduled_task_logs_desc'] = 'Die Überprüfung auf inaktive Nutzer wird von einem Moodle-Task im Hintergrund durchgeführt. Sollten Probleme mit dem Plugin auftrefen, können die technischen Logdaten der Moodle-Task-Ausführungen über den folgenden Button inspiziert werden.';
 
 // Errors.
-$string['error_invalid_config'] = 'Ungültige Plugin-Konfiguration gefunden.';
-$string['error_invalid_config_aborting'] = 'Ungültige Plugin-Konfiguration gefunden. Abbruch ...';
-$string['error_invalid_role_id'] = 'Ungültige Rollen-ID {$a} in der Liste der ignorierten Rollen gefunden. Dies ist höchstwahrscheinlich ein Fehler im Plugin. Bitte melden Sie dies dem Plugin-Maintainer.';
-$string['error_delete_threshold_days_negative'] = 'Die Löschschwelle muss größer als 0 sein.';
-$string['error_warning_threshold_days_negative'] = 'Die Warnschwelle muss größer als 0 sein.';
-$string['error_warning_threshold_days_geq_delete'] = 'Die Warnschwelle muss kleiner als die Löschschwelle sein.';
-$string['error_warning_email_subject_empty'] = 'Der Betreff der Warn-E-Mail darf nicht leer sein.';
-$string['error_warning_email_body_empty'] = 'Der Inhalt der Warn-E-Mail ist leer.';
-$string['error_delete_email_subject_empty'] = 'Der Betreff der Löschungs-E-Mail darf nicht leer sein.';
-$string['error_delete_email_body_empty'] = 'Der Inhalt der Löschungs-E-Mail ist leer.';
-$string['error_sending_warning_mail_to_user'] = 'Senden der Warn-E-Mail an Nutzer mit ID {$a} fehlgeschlagen.';
-$string['error_sending_delete_mail_to_user'] = 'Senden der Löschbenachrichtigung an Nutzer mit ID {$a} fehlgeschlagen.';
-$string['error_anonymizing_user'] = 'Anonymisieren des Nutzers mit ID {$a} fehlgeschlagen.';
-$string['error_deleting_user'] = 'Löschen des Nutzers mit ID {$a} fehlgeschlagen.';
--- lang/en/tool_userautodelete.php
+++ lang/en/tool_userautodelete.php
@@ -69,7 +69,6 @@ $string['filter'] = 'Filter';
 $string['filters'] = 'Filters';
 $string['action'] = 'Action';
 $string['actions'] = 'Actions';
-$string['lastmodifiedby'] = 'Last modified by';
 $string['add_action'] = 'Add action';
 $string['add_action_desc'] = 'Click on the type of action you want to add to the workflow step.';
 $string['add_filter'] = 'Add filter';
@@ -122,102 +121,23 @@ $string['newworkflow_desc'] = 'This is a freshly created workflow';
 
 // Task execution.
 $string['plugin_disabled_skipping_execution'] = 'Plugin is disabled globally, skipping execution.';
-$string['warning_email_disabled'] = 'Warning emails are disabled.';
-$string['warning_email_disabled_skipping'] = 'Sending warning emails is disabled, skipping step ...';
-$string['warning_email_sent_to_user'] = 'Warning email sent to user with ID {$a}';
-$string['delete_email_sent_to_user'] = 'Deletion notification email sent to user with ID {$a}';
-$string['user_anonymized'] = 'User with ID {$a} was anonymized';
-$string['user_deleted'] = 'User with ID {$a} was deleted';
-$string['user_recovered'] = 'User with ID {$a} was previously flagged as inactive but returned. User will not be deleted.';
 $string['users_to_warn'] = 'Users to warn';
-$string['users_to_warn_a'] = 'Found {$a} users eligible for a warning email.';
-$string['no_users_to_warn'] = 'No users eligible for a warning email found.';
 $string['users_to_delete'] = 'Users to delete';
-$string['users_to_delete_a'] = 'Found {$a} users eligible for deletion.';
-$string['no_users_to_delete'] = 'No users eligible for deletion found.';
 
 // Admin settings.
 $string['setting_plugin_desc'] = 'This plugin automatically deletes users that have not logged in for a configurable number of days. This is useful to keep your database clean and remove old accounts that are no longer needed. The plugin can be configured to send warning emails a number of days before the user is deleted. This gives users the chance to log back in and keep their accounts active. It furthermore supports deleting users in a GDPR-compliant way, leaving no trace of personally identifiable information (PII) inside the user record.';
 $string['setting_enable'] = 'Enable plugin';
 $string['setting_enable_desc'] = 'Enables or disables the plugin globally. If this is disabled, no action will be performed.';
-$string['setting_ignore_auths'] = 'Ignored auths';
-$string['setting_ignore_auths_desc'] = 'All users that use one of the selected authentication methods will never be deleted. Note that only enabled auth plugins are listed here.';
-$string['setting_ignore_siteadmins'] = 'Ignore site admins';
-$string['setting_ignore_siteadmins_desc'] = 'Global site admins can not be deleted. Revoke the admin role to include in automatic deletion process.';
-$string['setting_ignore_roles'] = 'Ignored roles';
-$string['setting_ignore_roles_desc'] = 'All users that have at least one of the selected roles assigned will never be deleted.';
-$string['setting_dryrun'] = 'Dry-run';
-$string['setting_dryrun_desc'] = 'A dry-run allows you to list all users that would receive a warning message or would be deleted during the next check interval. This is useful to verify that the plugin is configured correctly and to see which users would be affected. The dry-run does not perform any actions and therefore is fully non-destructive!';
-$string['setting_dryrun_button'] = 'Perform dry-run';
-$string['setting_suspended_only'] = 'Suspended users only';
-$string['setting_suspended_only_desc'] = 'If enabled, only users that are currently suspended will be considered for automatic deletion. All active users will be kept indefinitely unless this option is disabled. This can be useful if your user lifecycle is externally managed (e.g., via SSO) or contains a suspension phase before the user is finally deleted.';
 $string['setting_task_execution_interval'] = 'Check interval';
 $string['setting_task_execution_interval_desc'] = 'The check for inactive users is performed using a scheduled task that is executed via the Moodle cron. You can configure the interval at which it runs by clicking the following button.';
 $string['setting_task_execution_interval_button'] = 'Configure check interval';
-$string['setting_task_logs'] = 'Action Log';
-$string['setting_task_logs_desc'] = 'This plugin logs all actions it performs (e.g., user deletion). You can access the logs of all previously performed actions by clicking the following button.';
-$string['setting_task_logs_button'] = 'View action log';
 
-$string['setting_header_user_deletion'] = 'User deletion';
-$string['setting_header_user_deletion_desc'] = 'Configuration of the automatic user deletion. Users that have not logged in for at least the configured number of days and are not ignored by the above settings will be automatically deleted.';
-$string['setting_delete_threshold_days'] = 'Deletion threshold';
-$string['setting_delete_threshold_days_desc'] = 'The number of days of inactivity after which a user will be deleted. The deletion will only be performed if the user has not logged in for this number of days.';
-$string['setting_anonymize_user_data'] = 'Anonymize deleted users';
-$string['setting_anonymize_user_data_desc'] = 'When deleting a user, Moodle retains some parts of the user record inside the database by design. This includes first name, last name, e-mail address, last IP address, and other sensitive information. This may be a concern, depending on the specific data privacy policies of your organization or local legal interpretations. If this setting is enabled, all remaining user data will be anonymized, hereby fully eliminating the possibility trace back a deleted user account to the previous owner.';
-$string['setting_delete_email_enable'] = 'Deletion notifications';
-$string['setting_delete_email_enable_desc'] = 'If enabled, users will receive a final mail and are immediately deleted afterwards. This can be used to send confirmations of user deletion.';
-$string['setting_delete_email_subject'] = 'Mail subject';
-$string['setting_delete_email_subject_desc'] = 'The subject of the deletion notification email.';
-$string['setting_delete_email_subject_default'] = 'Your account was deleted';
-$string['setting_delete_email_body'] = 'Mail body';
-$string['setting_delete_email_body_desc'] = 'The body of the deletion notification email.';
-$string['setting_delete_email_body_default'] = '<p>Hello,</p><p>your account on our site was deleted due to inactivity. If you wish to continue using our service, please create a new account.</p><p>Kind regards</p>';
 
-$string['setting_header_deletion_warning'] = 'Deletion warnings';
-$string['setting_header_deletion_warning_desc'] = 'Configuration of the deletion warning emails. Warning emails are sent a number of days before the user is deleted. This informs users that their account will be deleted soon and gives them time to perform a login to keep their accounts active.';
-$string['setting_warning_email_enable'] = 'Deletion warnings';
-$string['setting_warning_email_enable_desc'] = 'If enabled, users will receive a warning email a configurable number of days before they are deleted.';
-$string['setting_warning_threshold_days'] = 'Warning threshold';
-$string['setting_warning_threshold_days_desc'] = 'The number of days before the user is deleted that the warning email will be sent. This is relative to the deletion threshold.';
-$string['setting_warning_email_subject'] = 'Mail subject';
-$string['setting_warning_email_subject_desc'] = 'The subject of the warning email.';
-$string['setting_warning_email_subject_default'] = 'Your account will be deleted soon - Action required!';
-$string['setting_warning_email_body'] = 'Mail body';
-$string['setting_warning_email_body_desc'] = 'The body of the warning email.';
-$string['setting_warning_email_body_default'] = '<p>Hello,</p><p>your account on our site has been inactive for a long period of time. To keep your account, please <strong>log back in now to prevent your account from being deleted</strong> according to our data protection policy within the next days.</p><p>If you wish your account to be deleted, you can ignore this message.</p><p>Kind regards</p>';
 
 // Page: info / dryrun
 $string['dry_run'] = 'Dry-run';
-$string['page_title_dryrun'] = 'Automatic user deletion (dry-run)';
 $string['dry_run_explanation'] = '<p>Below you will find all users that would be ingested by this workflow upon execution. None of the users is actually ingested at this point, so you can safely inspect the list to verify that the workflow is configured correctly.</p><p>Please note, that a single user can only be part of a single workflow at any point in time. Therefore, only users that match the defined filter criteria of the first workflow step and are not yet part of any workflow are listed below.</p>';
-$string['current_configuration'] = 'Current configuration';
-$string['current_configuration_table_desc'] = 'This table lists the next time a check for inactive users would be executed in the background as well as the configured thresholds for warning e-mails and user deletions. Additionally, user roles that are excluded from the deletion process are listed here.';
-$string['dry_run_affected_users_desc'] = 'Users listed here will either receive a warning email or will be deleted right away. If no users are listed, no user has been inactive for longer than the configured thresholds, or the user is ignored according to the plugin configuration. Accounts that have been created but to which no user ever logged in are listed with their creation date <i>"Time created"</i> instead of the last login date <i>"Last access"</i>.';
-$string['a_users_would_be_deleted'] = 'The following {$a} users would be deleted';
-$string['a_users_would_be_warned'] = 'The following {$a} users would receive a warning email';
 
 // Page: action logs
-$string['action_log'] = 'Action log';
-$string['action_log_explanation'] = 'This page lists all actions that have been performed by the automatic user deletion plugin. Every time a warning e-mail is sent, a user is finally deleted, or a user was marked as recovered after a previously sent warning e-mail, an entry in this action log is created. User inactivity checks that did not perform any actions are not logged.';
-$string['inspecting_raw_logs'] = 'Inspecting raw logs';
-$string['latest_actions'] = 'Latest actions';
-$string['latest_actions_desc'] = 'This table lists the latest actions that have been performed by the automatic user deletion plugin. Checks without any action are excluded.';
-$string['page_title_action_log'] = 'Automatic user deletion (action log)';
-$string['view_scheduled_task_logs'] = 'View raw scheduled task logs';
-$string['view_scheduled_task_logs_desc'] = 'The checks for inactive users are performed by a Moodle scheduled task, running in the background. If you experience problems with this plugin, you can inspect the raw logs of the scheduled task runs by clicking the button below.';
 
 // Errors.
-$string['error_invalid_config'] = 'Invalid plugin configuration found.';
-$string['error_invalid_config_aborting'] = 'Invalid plugin configuration found. Aborting ...';
-$string['error_invalid_role_id'] = 'Invalid role ID {$a} found in the ignored roles list. This is most likely a bug in the plugin. Please report this to the plugin maintainer.';
-$string['error_delete_threshold_days_negative'] = 'The deletion threshold days must be greater than 0.';
-$string['error_warning_threshold_days_negative'] = 'The warning threshold days must be greater than 0.';
-$string['error_warning_threshold_days_geq_delete'] = 'The warning threshold days must be less than the deletion threshold days.';
-$string['error_warning_email_subject_empty'] = 'The warning email subject must not be empty.';
-$string['error_warning_email_body_empty'] = 'The warning email body is empty.';
-$string['error_delete_email_subject_empty'] = 'The deletion email subject must not be empty.';
-$string['error_delete_email_body_empty'] = 'The deletion email body is empty.';
-$string['error_sending_warning_mail_to_user'] = 'Sending warning email to user with ID {$a} failed.';
-$string['error_sending_delete_mail_to_user'] = 'Sending deletion notification email to user with ID {$a} failed.';
-$string['error_anonymizing_user'] = 'Anonymizing user with ID {$a} failed.';
-$string['error_deleting_user'] = 'Deleting user with ID {$a} failed.';
