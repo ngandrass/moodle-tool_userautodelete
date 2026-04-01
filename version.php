bdea5410e1a18a34460b22cc1d217bb2b87794e7
Niels Gandraß <niels@gandrass.de>
2026-04-01 15:47:34 +0200

    Update copyright notice to 2026

--- version.php
+++ version.php
@@ -18,7 +18,7 @@
  * Plugin version and other meta-data are defined here.
  *
  * @package     tool_userautodelete
- * @copyright   2025 Niels Gandraß <niels@gandrass.de>
+ * @copyright   2026 Niels Gandraß <niels@gandrass.de>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
