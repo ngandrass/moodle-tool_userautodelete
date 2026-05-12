49ef14ad5334e61e330f45579b2a428eb13648c6
Niels Gandraß <niels@gandrass.de>
2026-05-12 19:36:13 +0200

    action_suspend, action_unsuspend: Bump to v1.1.0 (2026051200)

--- action/unsuspend/version.php
+++ action/unsuspend/version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
 
 $plugin->component = 'userdeleteaction_unsuspend';
-$plugin->release = '1.0.0';
-$plugin->version = 2026050500;
+$plugin->release = '1.1.0';
+$plugin->version = 2026051200;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 502]; // X meta-supported-moodle{4.5 - 5.2} meta-supported-php{8.1 - 8.4}.
 $plugin->maturity = MATURITY_STABLE;
