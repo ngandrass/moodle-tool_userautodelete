29e308f31d1b6c53252c601e71c95d01ffbf2084
Niels Gandraß <niels@gandrass.de>
2026-05-13 08:58:59 +0200

    action_anonymize: Bump to v1.1.1

--- action/anonymize/version.php
+++ action/anonymize/version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
 
 $plugin->component = 'userdeleteaction_anonymize';
-$plugin->release = '1.1.0';
-$plugin->version = 2026051200;
+$plugin->release = '1.1.1';
+$plugin->version = 2026051300;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 502]; // X meta-supported-moodle{4.5 - 5.2} meta-supported-php{8.1 - 8.4}.
 $plugin->maturity = MATURITY_STABLE;
