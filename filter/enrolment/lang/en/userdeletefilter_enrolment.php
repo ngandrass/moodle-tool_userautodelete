a5f09fab46b405633e235f6a399e3961b0436067
Richard Aljaste <richard.aljaste@ut.ee>
2026-07-23 10:06:36 +0300

    Add enrolment filter subplugin.

---
+++ filter/enrolment/lang/en/userdeletefilter_enrolment.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     userdeletefilter_enrolment
+ * @category    string
+ * @author      Richard Aljaste <richard.aljaste@ut.ee>
+ * @copyright   2026 University of Tartu <veebitiim@ut.ee>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// @codingStandardsIgnoreFile
+
+defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
+
+$string['pluginname'] = 'Course enrolment';
+$string['privacy:metadata'] = 'This plugin does not store any personal data.';
+$string['setting_enrolled'] = 'User course enrollment status';
+$string['setting_enrolled_help'] = 'Select whether the user MUST or MUST NOT be enrolled to some course.';
+$string['enrolled'] = 'Must be enrolled to at least one course';
+$string['not_enrolled'] = 'Must not be enrolled to any courses';
