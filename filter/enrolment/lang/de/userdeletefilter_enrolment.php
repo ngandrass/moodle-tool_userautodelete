f1694fab5fedca2121495b826344108e6094555a
Niels Gandraß <niels@gandrass.de>
2026-07-23 15:00:44 +0200

    filter_enrolment: Add German languange strings

---
+++ filter/enrolment/lang/de/userdeletefilter_enrolment.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     userdeletefilter_enrolment
+ * @category    string
+ * @author      Niels Gandraß <niels@gandrass.de>
+ * @copyright   2026 Niels Gandraß <niels@gandrass.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// @codingStandardsIgnoreFile
+
+defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
+
+$string['pluginname'] = 'Kurseinschreibung';
+$string['privacy:metadata'] = 'Dieses Plugin speicher keine personenbezogenen Daten';
+$string['setting_enrolled'] = 'Status der Einschreibung';
+$string['setting_enrolled_help'] = 'Wählen Sie aus, ob der Benutzer in mindestens einen Kurs eingeschrieben sein muss oder nicht.';
+$string['enrolled'] = 'Muss in mindestens einen Kurs eingeschrieben sein';
+$string['not_enrolled'] = 'Darf in keinen Kurs eingeschrieben sein';
