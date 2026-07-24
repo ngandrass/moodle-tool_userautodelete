3ac457838379c646d8051d7e8c5e543eae98b7c8
Niels Gandraß <niels@gandrass.de>
2026-07-24 09:19:40 +0200

    action_cohort: Add German translation

---
+++ action/cohort/lang/de/userdeleteaction_cohort.php
@@ -0,0 +1,39 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * Plugin strings are defined here.
+ *
+ * @package     userdeleteaction_cohort
+ * @category    string
+ * @copyright   2026 Niels Gandraß <niels@gandrass.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+// @codingStandardsIgnoreFile
+
+defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
+
+$string['pluginname'] = 'Kohorte';
+$string['privacy:metadata'] = 'Dieses Plugin speichert keine personenbezogenen Daten.';
+$string['details_mode_add'] = 'Hinzufügen zu:';
+$string['details_mode_remove'] = 'Entfernen aus:';
+$string['setting_cohortids'] = 'Kohorten';
+$string['setting_cohortids_help'] = 'Wählen Sie eine oder mehrere Kohorten (Globale Gruppen) aus, zu denen Nutzer hinzugefügt oder aus denen sie entfernt werden.';
+$string['setting_mode'] = 'Modus';
+$string['setting_mode_help'] = 'Wählen Sie, ob Nutzer den ausgewählten Kohorten (Globale Gruppen) hinzugefügt oder aus diesen entfernt werden sollen.';
+$string['setting_mode_add'] = 'Nutzer zu Kohorte(n) hinzufügen';
+$string['setting_mode_remove'] = 'Nutzer aus Kohorte(n) entfernen';
