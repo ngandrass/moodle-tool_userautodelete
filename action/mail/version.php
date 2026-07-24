851443076b6b1fa0c1084c7b5e4eb98d943aa5e0
Niels Gandraß <niels@gandrass.de>
2026-07-24 17:55:21 +0200

    action_mail: Raise version number

--- action/mail/version.php
+++ action/mail/version.php
@@ -26,8 +26,8 @@
 defined('MOODLE_INTERNAL') || die(); // @codeCoverageIgnore
 
 $plugin->component = 'userdeleteaction_mail';
-$plugin->release = '1.1.0';
-$plugin->version = 2026051800;
+$plugin->release = '1.2.0';
+$plugin->version = 2026072400;
 $plugin->requires = 2024100700;
 $plugin->supported = [405, 502]; // X meta-supported-moodle{4.5 - 5.2} meta-supported-php{8.1 - 8.4}.
 $plugin->maturity = MATURITY_STABLE;
