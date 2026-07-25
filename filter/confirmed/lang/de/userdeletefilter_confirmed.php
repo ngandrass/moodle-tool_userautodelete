09bbd2d8c4c9fabc41fc598517460456fe440f34
Niels Gandraß <niels@gandrass.de>
2026-07-25 09:42:41 +0200

    filter_confirmed: Create filter sub-plugin that allows to target (un-)confirmed user accounts

---
+++ filter/confirmed/lang/de/userdeletefilter_confirmed.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * German language strings for the userdeletefilter_confirmed sub-plugin.
+ *
+ * @package     userdeletefilter_confirmed
+ * @copyright   2026 Niels Gandraß <niels@gandrass.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['missing_mode_setting'] = 'Die erforderliche Einstellung für den Bestätigungsmodus wurde nicht konfiguriert.';
+$string['mode_confirmed'] = 'Bestätigt';
+$string['mode_unconfirmed'] = 'Nicht bestätigt';
+$string['pluginname'] = 'Kontobestätigung';
+$string['privacy:metadata'] = 'Dieses Plugin speichert keine personenbezogenen Daten.';
+$string['setting_mode'] = 'Bestätigungsstatus';
+$string['setting_mode_help'] = 'Wählen Sie aus, welche Nutzer anhand ihres Kontobestätigungsstatus ausgewählt werden sollen.<br><br>Bei <b>Nicht bestätigt</b> werden nur Nutzer ausgewählt, die ihr Konto noch nicht bestätigt haben (z.B. durch die E-Mail-Bestätigung nach der Selbstregistrierung).<br><br>Bei <b>Bestätigt</b> werden nur Nutzer ausgewählt, deren Konto bereits bestätigt ist.';
---
+++ filter/confirmed/lang/en/userdeletefilter_confirmed.php
@@ -0,0 +1,31 @@
+<?php
+// This file is part of Moodle - https://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <https://www.gnu.org/licenses/>.
+
+/**
+ * English language strings for the userdeletefilter_confirmed sub-plugin.
+ *
+ * @package     userdeletefilter_confirmed
+ * @copyright   2026 Niels Gandraß <niels@gandrass.de>
+ * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['missing_mode_setting'] = 'The required confirmation mode setting has not been configured.';
+$string['mode_confirmed'] = 'Confirmed';
+$string['mode_unconfirmed'] = 'Unconfirmed';
+$string['pluginname'] = 'Account Confirmation';
+$string['privacy:metadata'] = 'This plugin does not store any personal data.';
+$string['setting_mode'] = 'Confirmation state';
+$string['setting_mode_help'] = 'Select which users should be targeted based on their account confirmation state.<br><br>If set to <b>Unconfirmed</b>, only users who have not yet confirmed their account (e.g. by verifying their email address after self-registration) will be affected.<br><br>If set to <b>Confirmed</b>, only users whose account is already confirmed will be affected.';
